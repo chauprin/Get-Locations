c53b5b03fe775bf98c8177444b89b1e3b67baab3
ubuntu <princechauhan248008@gmail.com>
2017-05-15 20:44:26 +0530

    updated app.py

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ There are four endpoints
 http://localhost:5000/register/arguments?application_name=APPLICATION_NAME&application_website=APPLICATION_WEBSITE    
 2. Post Location : The clients can post data about location using this endpoint by using the Client   Credentials    
 http://localhost:5000/post_location/arguments?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&place=PLACE_NAME&lat=LATITUDE&lon=LONGITUDE    
-3. Get Using Postgres : Gives places inside 5 km of the hardcoded latitude and longitude (30.31, 78.03) values by using extensions of PostgreSQL    
+3. Get Using Postgres : Gives places inside 5 km of the hardcoded latitude and longitude (45.12, 71.12) values by using extensions of PostgreSQL    
 http://localhost:5000/get_using_postgres/arguments?client_id=CLIENT_ID&client_secret=CLIENT_SECRET    
-4. Get Using Self : Also gives places inside 5 km of the hardcoded latitude and longitude (30.31, 78.03) values by mathematical calculations     
+4. Get Using Self : Also gives places inside 5 km of the hardcoded latitude and longitude (45.12, 71.12) values by mathematical calculations     
 http://localhost:5000/get_using_self/arguments?client_id=CLIENT_ID&client_secret=CLIENT_SECRET 
 
 
--- app.py
+++ app.py
@@ -21,12 +21,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
 %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
 db.init_app(app)
 
-#Cursor for using database
+#Created a cursor for using database
 conn = psycopg2.connect(database = 'my_db1', user = 'postgres', password = 'password', host = 'localhost')
 curs = conn.cursor()
 
-latitude = 30.31
-longitude = 78.03
+#Hardcoded latitude and longitude values
+latitude = 45.12 
+longitude = 71.12
 
 #Home
 @app.route('/')
@@ -37,16 +38,25 @@ def home():
 @app.route('/register/arguments', methods = ['GET', 'POST'])
 def register():
 	if request.method == "POST":
+		
+		#Postgresql gives frequent errors if this commit is not done 
 		conn.commit()
+		
 		application_name = request.args.get("application_name")
 		application_website = request.args.get("application_website")
-		client_id = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
-		client_secret = ''.join(random.choice('0123456789ABCDEF') for i in range(16))    	
+
+		#Creating a 16 digit random client_id and client_secret 
+		client_id = ''.join(random.choice('0123456789ABCDEF') for i in range(16)) 
+		client_secret = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
+
+		#Preventing empty field values
 		if application_name == "" or application_website == "":
 			return jsonify("Empty fields are not allowed")
+		
+		#The recieved values using register are inserted into clients table
 		curs.execute("insert into clients (application_name, application_website, client_id, client_secret) values(%s, %s, %s, %s);",[application_name, application_website, client_id, client_secret])
 		conn.commit()
-		#message = "Your CLIENT_ID = %s and your CLIENT_SECRET = %s. Please note these. You are gonna need them for using this api" %(client_id, client_secret)
+
 		return jsonify({"Note": "Please note the client_id and client_secret. You will need them for get requests.",
 						"Credentials" :	{"application_name" : application_name, 
 										"application_website" : application_website,
@@ -64,16 +74,24 @@ def register():
 def post_location():
 	if request.method == "POST":
 		conn.commit()
+		
 		client_id = request.args.get("client_id")
 		client_secret = request.args.get("client_secret")
+		
+		#Fetching client_secret for authentication
 		curs.execute("select client_secret from clients where client_id = %s;",[client_id])
 		all_results = curs.fetchone()
+		
 		if client_secret == all_results[0]:
 			place = request.args.get("place")
 			lat = request.args.get("lat")
 			lon = request.args.get("lon")
+			
+			#Preventing empty fields
 			if lat == "" or lon == "" or place == "":
 				return jsonify("Empty fields not allowed")	
+			
+			#The recieved values using post are inserted into my_points table
 			curs.execute("insert into my_points (place, lat, lon) values(%s,%s,%s);",[place, lat, lon])
 			conn.commit()
 			return jsonify("Row Inserted.")
@@ -82,18 +100,24 @@ def post_location():
 	else:
 		return jsonify("Bad request. Only POST requests are allowed on this endpoint.")
 
-#GET_USING_POSTGRES API
+#GET_USING_POSTGRES 
 @app.route("/get_using_postgres/arguments")
 def get_using_postgres():
 	client_id = request.args.get("client_id")
 	client_secret = request. args.get("client_secret")
+	
+	#Fetching client_secret for authentication
 	curs.execute("select client_secret from clients where client_id = %s;",[client_id])
 	all_results = curs.fetchone()
+	
 	if client_secret == all_results[0]:
 		conn.commit()
+		
+		#Calculating time taken for execution of query for comparison purposes
 		start_time = timeit.default_timer()
 		curs.execute("select * from my_points where (point(lon,lat) <@> point(%s,%s)) <= 5/1.6;",[longitude,latitude])
 		elapsed = timeit.default_timer() - start_time
+		
 		print "***--- Time taken for get_using_postgres = %s ---***" %(elapsed)
 		rows=curs.fetchall()
 		return jsonify({"Time taken" : elapsed,
@@ -106,14 +130,19 @@ def get_using_postgres():
 def get_using_self():
 	client_id = request.args.get("client_id")
 	client_secret = request. args.get("client_secret")
+	
+	#Fetching client_secret for authentication
 	curs.execute("select client_secret from clients where client_id = %s;",[client_id])
 	all_results = curs.fetchone()
 
 	if client_secret == all_results[0]:
 		conn.commit()
+		
+		#Calculating execution time for comparison purposes
 		start_time = timeit.default_timer()
 		curs.execute("select * from ( select s_no, place, lat, lon, (3959 * acos (cos ( radians(%s) ) * cos( radians( lat ) ) * cos( radians( lon ) - radians(%s) ) + sin ( radians(%s) ) * sin( radians( lat ) ) ) ) AS distance FROM my_points order by distance) items where distance < 5/1.6;",[latitude,longitude,latitude])
 		elapsed = timeit.default_timer() - start_time
+		
 		print "***--- Time taken for get_using_self = %s ---***" %(elapsed)
 		rows = curs.fetchall()
 		return jsonify({"Time taken" : elapsed,
